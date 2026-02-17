f6e988f595cfaf5017fe7089d0c9b83696c17ec4
AaronPiramide <230188@fppiramide.com>
2026-02-17 13:02:43 +0100

    formulario inicio sesion empezado

---
+++ src/componentes/Formularios/FormularioRegistro.jsx
@@ -0,0 +1,26 @@
+import App from "../../App";
+
+export function FormularioRegistro() {
+
+
+    return (
+
+        <>
+        
+        <form action="" method="post">
+
+
+
+        </form>
+
+        </>
+        
+    )
+
+
+}
+
+
+
+
+export default App
