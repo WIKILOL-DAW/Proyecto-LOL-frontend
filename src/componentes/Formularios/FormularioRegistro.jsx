b9333b1243f5ead69e1936444a20b7636a0f05a1
Aaron <230188@fppiramide.com>
2026-02-19 15:12:37 +0100

    Formulario registro, falta mirar porque no va el boton toggle

--- src/componentes/Formularios/FormularioRegistro.jsx
+++ src/componentes/Formularios/FormularioRegistro.jsx
@@ -1,19 +1,32 @@
 import { Boton } from "../Boton/Boton";
+import { NavBar } from "../NavBar/NavBar";
 
 export function FormularioRegistro() {
 
     return (
 
         <>
+            <NavBar />
+            <div className="hero bg-base-200 min-h-screen">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left">
+                    </div>
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                        <div className="card-body">
+                            <fieldset className="fieldset">
 
-            <h2>Bienvenido, introduzca sus credenciales para crear su nueva cuenta</h2>
-            <form action="" method="">
-                <input type="text" placeholder="Indique un alias" /> <br /> <br />
-                <input type="email" placeholder="Correo electronico" /> <br /> <br />
-                <input type="password" placeholder="Contraseña" />  <br /> <br />
-                <Boton/>
-            </form>
+                                <input type="email" className="input" placeholder="Nombre de usuario" />
 
+                                <input type="email" className="input" placeholder="Email" />
+                                
+                                <input type="password" className="input" placeholder="Contraseña" />
+
+                                <button className="btn btn-neutral mt-4">Registrarse</button>
+                            </fieldset>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
