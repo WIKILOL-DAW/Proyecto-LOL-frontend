54c9fc2c13818b65f66f1f127b4ba930610bef66
Adrian Rami Piramide <230053@fppiramide.com>
2026-02-26 12:11:04 +0100

    noticia estructura, equipo intento

---
+++ src/componentes/Equipo/Equipo.jsx
@@ -0,0 +1,17 @@
+export default function equipo(){
+
+    const[equipo,setEquipo]={
+        nombre:"",
+        liga:"",
+        jugadores:""
+    }
+
+    return(
+        <>
+
+            <h1>Aqui va la informacion del Equipo</h1>
+
+
+        </>
+    )
+}
