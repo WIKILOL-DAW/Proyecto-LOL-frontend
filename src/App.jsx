b15e29247cf3edb14257ecede329b90b73ed7298
Aaron <230188@fppiramide.com>
2026-03-30 18:25:29 +0200

    Estilos petados

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { NavbarSelector } from "./componentes/NavBar/NavBarSelector"
-import { FormularioInicioSesion } from "./componentes/Formularios/FormularioInicioSesion"
-import { FormularioRegistro } from "./componentes/Formularios/FormularioRegistro"
-import Inicio from "./componentes/Inicio/Inicio"
+import { Routes, Route } from "react-router-dom";
+import { NavbarSelector } from "./componentes/NavBar/NavBarSelector";
+import { FormularioInicioSesion } from "./componentes/Formularios/FormularioInicioSesion";
+import { FormularioRegistro } from "./componentes/Formularios/FormularioRegistro";
+import Inicio from "./componentes/Inicio/Inicio";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
       <NavbarSelector />
 
-
       <Routes>
-        <Route path="/" element={<Inicio />} />
+       
         <Route path="/login" element={<FormularioInicioSesion />} />
         <Route path="/registro" element={<FormularioRegistro />} />
+         <Route path="/" element={<Inicio />} />
       </Routes>
-    </BrowserRouter>
-
-      <h1>Inicio</h1>
     </>
-
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router'
-import { FormularioRegistro } from './componentes/Formularios/FormularioRegistro.jsx'
-import { FormularioInicioSesion } from './componentes/Formularios/FormularioInicioSesion.jsx'
-import { Campeones } from './componentes/Campeones/Campeones.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-
-    <Routes>
-
-      <Route path='/' element={<App />} />
-      <Route path='/registro' element={<FormularioRegistro />} />
-      <Route path='/login' element={<FormularioInicioSesion />} />
-      <Route path='/campeones' element={<Campeones />} />
-
-    </Routes>
-
+    <App />
   </BrowserRouter>
-)
+);
