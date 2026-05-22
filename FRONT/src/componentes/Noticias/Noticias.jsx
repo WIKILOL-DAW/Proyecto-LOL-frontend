d379a6bac1b9b9c1342785cbcc2ef04e991dc309
Alex <240151@fppiramide.com>
2026-05-22 21:12:48 +0200

    FIN 22

--- FRONT/src/componentes/Noticias/Noticias.jsx
+++ FRONT/src/componentes/Noticias/Noticias.jsx
@@ -1,19 +1,19 @@
 import { useEffect, useState } from "react";
 import { get } from "../../servicios/Administrador.peticiones.service";
 
+//Falta modificar la vista ya que al final no se utiliza una S3 para almacenar las imagenes.
+//Hay que eliminar las imagenes de las noticias de cara al 2.
+
 export default function Noticias() {
 
     const [noticias, setNoticias] = useState([]);
 
     useEffect(() => {
-
         get(
             "/api/noticia/verNoticias",
-
             (data) => {
                 setNoticias(data.noticias);
             },
-
             (error) => {
                 console.log("Error cargando noticias:", error);
             }
@@ -26,7 +26,6 @@ export default function Noticias() {
             <h1 className="text-4xl font-bold text-yellow-400 mb-8 text-center drop-shadow-lg">
                 Últimas Noticias
             </h1>
-
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-6 bg-gray-900 rounded-xl shadow-xl">
                 {noticias.map((noticia) => (
                     <div key={noticia.id} className="card bg-base-200 shadow-xl border border-yellow-700 hover:border-yellow-400 transition-all duration-300 group">
@@ -42,17 +41,13 @@ export default function Noticias() {
                                 </span>
                             </div>
                         </figure>
-
                         <div className="card-body">
                             <p className="text-gray-300 text-sm leading-relaxed">
                                 {noticia.contenido}
                             </p>
                         </div>
-
                     </div>
-
                 ))}
-
             </div>
         </>
     );
