52c83429e0559272fdb2be9bb6f4bb1337229d6e
Adrianramipiramide <152392777+Adrianramipiramide@users.noreply.github.com>
2026-05-25 12:27:15 +0200

    Update dataRegular.js para despliegue

--- FRONT/src/componentes/Esports/dataRegular.js
+++ FRONT/src/componentes/Esports/dataRegular.js
@@ -5,7 +5,8 @@ export async function obtenerPartidasTorneo(campeonato) {
     try {
 
         const response = await fetch(
-            "http://localhost:8080/api/partida/obtenerPartidas"
+           // "http://localhost:8080/api/partida/obtenerPartidas"
+            http://lol-alb-2138982396.us-east-1.elb.amazonaws.com:80
         );
 
         const data = await response.json();
